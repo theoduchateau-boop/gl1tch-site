29c09337bacaa66f4276c99f1fdb8b22624c5c42
theoduchateau-boop <theo.duchateau@groupecat.com>
2026-03-12 09:59:18 +0100

    hero premium avec canette

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const btn = document.getElementById("dys");
 
 btn.onclick = () => {
-
   document.body.classList.toggle("font-dyslexic");
 
+  if (document.body.classList.contains("font-dyslexic")) {
+    btn.textContent = "Police normale";
+  } else {
+    btn.textContent = "Mode dyslexique";
+  }
 };
